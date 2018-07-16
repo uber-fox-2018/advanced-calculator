1d3a09bef3dcbad389e2239b8072d9f9f4a15426
arisupriatna14 <arisupriatna703@gmail.com>
2018-07-16 11:47:12 +0700

    done

--- main.js
+++ main.js
@@ -2,4 +2,18 @@
 
 const Calculator = require("./calculator.js")
 
-// execute function on calculator.js in here
+const calculators = new Calculator(5)
+  .add(5)
+  .multiply(10)
+  .divide(10)
+  .square(5)
+console.log(calculators.result())
+
+const akar = new Calculator()
+  .squareRoot(100)
+console.log(akar.result())
+
+const luas_lingkaran = new Calculator()
+  .luasLingkaran(9)
+console.log(luas_lingkaran.result())
+
