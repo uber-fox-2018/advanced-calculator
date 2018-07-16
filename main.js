aab8c5b4e4c813dc11574c67f194991a7de1aec6
harlesbayu <bayuanggaraharles@gmail.com>
2018-07-16 12:51:38 +0700

    calculator solved

--- main.js
+++ main.js
@@ -2,4 +2,9 @@
 
 const Calculator = require("./calculator.js")
 
+var count = new Calculator(4)
+
+console.log(count.add(3).multiply(7).subtract(19).divide(10).square(3).result)
+console.log(count.add(5).result)
+
 // execute function on calculator.js in here
