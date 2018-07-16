627fef9e08e8879e3b3a2f9f48a2bad2b5cd3a6e
Khodhi Robbani <khodhirobbani@gmail.com>
2018-07-16 11:08:13 +0700

    kalkulator

--- main.js
+++ main.js
@@ -3,3 +3,6 @@
 const Calculator = require("./calculator.js")
 
 // execute function on calculator.js in here
+let angkaPertama =  5
+let kalkulator = new Calculator (angkaPertama)
+console.log(kalkulator.add(6).square(2).squareRoot().result())
