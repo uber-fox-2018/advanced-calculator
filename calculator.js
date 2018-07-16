22342bfe86485d91673d25f3e0968938020e4b66
Grayfield <grayfieldmm@outlook.com>
2018-07-16 10:51:03 +0700

    Finished calculator challenge

--- calculator.js
+++ calculator.js
@@ -2,19 +2,35 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (number = 0) {
+    this.data = number;
   }
-  add () {
+  add (number) {
+    this.data += number;
+    return this;
   }
-  subtract () {
+  subtract (number) {
+    this.data -= number;
+    return this;
   }
-  multiply () {
+  multiply (number) {
+    this.data *= number;
+    return this;
   }
-  divide () {
+  divide (number) {
+    this.data /= number;
+    return this;
   }
-  square () {
+  square (number) {
+    this.data = Math.pow(this.data, number);
+    return this;
   }
   squareRoot () {
+    this.data = Math.sqrt(this.data);
+    return this;
+  }
+  static get PI() {
+    return Math.PI;
   }
 }
 
--- main.js
+++ main.js
@@ -3,3 +3,13 @@
 const Calculator = require("./calculator.js")
 
 // execute function on calculator.js in here
+let calc1 = new Calculator(2); //2
+calc1.square(8) //2^8 = 256
+     .squareRoot() //sqrt 256 = 16
+     .add(8) //16 + 8 = 24
+     .subtract(20) //24 - 20 = 4
+     .multiply(Calculator.PI) //4 * 3 = 12
+     .divide(2);
+
+     
+console.log(calc1['data']);
